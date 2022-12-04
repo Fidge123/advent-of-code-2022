f2e80580b7a6052ca4245a9ee4cbcf93ff89a1af
Florian Richter <flori@hey.com>
2022-12-04 14:01:53 +0100

    Improve day 4

--- 4-1.py
+++ 4-1.py
@@ -1,16 +1,14 @@
 from aocd import get_data, submit
 
-def parse_assignment(assignment: str) -> list[list[str]]:
-    return [elf.split('-') for elf in assignment.split(',')]
+def parse_assignment(assignment: str) -> list[list[int]]:
+    return [list(map(int, elf.split('-'))) for elf in assignment.split(',')]
 
 assignments = [parse_assignment(assignment) for assignment in get_data(day=4, year=2022).splitlines()]
 
 count = 0
 
-for index, (elf1, elf2) in enumerate(assignments):
-    start1, end1 = map(int, elf1)
-    start2, end2 = map(int, elf2)
+for index, ((start1, end1), (start2, end2)) in enumerate(assignments):
     if (start1 <= start2 and end1 >= end2) or (start2 <= start1 and end2 >= end1):
         count += 1
 
-submit(count)
+print(count)
