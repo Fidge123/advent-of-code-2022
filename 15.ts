614c1040681ea8301aada12ce42344335d001f45
Florian Richter <flori@hey.com>
2022-12-15 19:55:31 +0100

    Add solution for day 15 part 2

--- 15.ts
+++ 15.ts
@@ -1,5 +1,11 @@
 import { runPart } from "https://deno.land/x/aocd/mod.ts";
 
+interface Range {
+  start: number;
+  end: number;
+  gaps?: Range[];
+}
+
 function parse(input: string) {
   return [
     ...input.matchAll(
@@ -20,8 +26,7 @@ function parse(input: string) {
       beaconX,
       beaconY,
       distance: manhattan(sensorX, sensorY, beaconX, beaconY),
-    }))
-    .sort((a, b) => b.distance - a.distance);
+    }));
 }
 
 function manhattan(
@@ -33,39 +38,64 @@ function manhattan(
   return Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
 }
 
-function getBlocked(sensorData: Record<string, number>[], line: number) {
-  const blocked = new Set();
+function getBlocked(sensorData: Record<string, number>[], line: number): Range {
+  return sensorData
+    .map(({ sensorX, sensorY, distance }): Range | undefined => {
+      const d = distance - Math.abs(line - sensorY);
+      if (d > 0) {
+        return {
+          start: sensorX - d,
+          end: sensorX + d,
+        };
+      }
+    })
+    .filter((range): range is Range => typeof range !== "undefined")
+    .sort((a, b) => a.start - b.start)
+    .reduce((prev, curr) => {
+      if (curr.start - prev.end > 1) {
+        prev.gaps = [
+          ...(prev.gaps ?? []),
+          { start: prev.end + 1, end: curr.start - 1 },
+        ];
+      }
+      prev.end = Math.max(prev.end, curr.end);
+      return prev;
+    });
+}
 
-  for (const { sensorX, sensorY, distance } of sensorData) {
-    const blockDistance = distance - Math.abs(line - sensorY);
-
-    for (let i = 0; i <= blockDistance; i++) {
-      blocked.add(sensorX + i);
-      blocked.add(sensorX - i);
-    }
-  }
-  return blocked;
+function calc(range: Range): number {
+  return (
+    1 +
+    range.end -
+    range.start -
+    (range.gaps?.reduce((sum, gap) => sum + gap.end - gap.start, 0) ?? 0)
+  );
 }
 
 function part1(input: string) {
   const sensorData = parse(input);
-  const blocked = getBlocked(sensorData, 2_000_000);
-  for (const { beaconX, beaconY } of sensorData) {
-    if (beaconY === 2_000_000) {
-      blocked.delete(beaconX);
-    }
-  }
-  return blocked.size;
+  return (
+    calc(getBlocked(sensorData, 2_000_000)) -
+    new Set(
+      sensorData
+        .filter(({ beaconY }) => beaconY === 2_000_000)
+        .map(({ beaconX }) => beaconX)
+    ).size
+  );
 }
 
 function part2(input: string) {
   const sensorData = parse(input);
-  const area = [...Array(4_000_000)].map(() =>
-    [...Array(4_000_000)].map(() => true)
-  );
+  for (let y = 0; y <= 4_000_000; y++) {
+    const b = getBlocked(sensorData, y);
+    if (b.gaps?.length > 0 || b.start > 0 || b.end < 4_000_000) {
+      return b.gaps![0].start * 4_000_000 + y;
+    }
+  }
+  return -1;
 }
 
 if (import.meta.main) {
   runPart(2022, 15, 1, part1);
-  // runPart(2022, 15, 2, part2);
+  runPart(2022, 15, 2, part2);
 }
