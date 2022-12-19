b00484e778998b461fde716cd1b4aefd2e9afb3a
Florian Richter <flori@hey.com>
2022-12-19 19:02:47 +0100

    Add day 18 part 2

--- 18.ts
+++ 18.ts
@@ -23,18 +23,93 @@ function adjacent([x, y, z]: ThreeD): ThreeD[] {
   ];
 }
 
+function isInScan(cube: ThreeD, scan: ThreeD[]) {
+  return scan.some((block) => block.every((s, i) => s === cube[i]));
+}
+
 function part1(input: string) {
   return parse(input.trim()).reduce(
-    (sum, cube, _, arr) =>
-      sum +
-      adjacent(cube).filter(
-        (side) => !arr.some((block) => block.every((s, i) => s === side[i]))
-      ).length,
+    (sum, cube, _, scan) =>
+      sum + adjacent(cube).filter((side) => !isInScan(side, scan)).length,
     0
   );
 }
 
+function isObsidianOrPocket(
+  cube: ThreeD,
+  scan: ThreeD[],
+  visited: ThreeD[] = [],
+  prefix = ""
+): boolean {
+  if (isInScan(cube, scan)) {
+    return true;
+  }
+  if (isInScan(cube, visited)) {
+    return true;
+  }
+  if (allAir(cube, scan)) {
+    return false;
+  }
+  const isOOP = adjacent(cube)
+    .filter((c) => !isInScan(c, visited))
+    .every((side) =>
+      isObsidianOrPocket(
+        side,
+        scan,
+        [...pockets, ...visited, cube],
+        prefix + "  "
+      )
+    );
+  if (isOOP) {
+    pockets.push(cube);
+  }
+  return isOOP;
+}
+
+function allAir(cube: ThreeD, scan: ThreeD[]) {
+  return !(
+    scan.some(
+      (block) =>
+        block[0] === cube[0] && block[1] === cube[1] && block[2] > cube[2]
+    ) &&
+    scan.some(
+      (block) =>
+        block[0] === cube[0] && block[1] === cube[1] && block[2] < cube[2]
+    ) &&
+    scan.some(
+      (block) =>
+        block[0] === cube[0] && block[1] > cube[1] && block[2] === cube[2]
+    ) &&
+    scan.some(
+      (block) =>
+        block[0] === cube[0] && block[1] < cube[1] && block[2] === cube[2]
+    ) &&
+    scan.some(
+      (block) =>
+        block[0] > cube[0] && block[1] === cube[1] && block[2] === cube[2]
+    ) &&
+    scan.some(
+      (block) =>
+        block[0] < cube[0] && block[1] === cube[1] && block[2] === cube[2]
+    )
+  );
+}
+
+const pockets = [] as ThreeD[];
+
+function part2(input: string) {
+  return parse(input.trim()).reduce((sum, cube, _, scan) => {
+    const surface = adjacent(cube).filter((side) => {
+      const foo = isObsidianOrPocket(side, scan, pockets);
+
+      return !foo;
+    }).length;
+
+    return sum + surface;
+  }, 0);
+}
+
 if (import.meta.main) {
   runPart(2022, 18, 1, part1);
-  // runPart(2022, 18, 2, part2);
+  runPart(2022, 18, 2, part2);
 }
